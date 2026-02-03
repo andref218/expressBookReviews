56c3a78fc927a8f27a0a9284629816a4ec616ef1
Andre Fonseca <andrefonseca.aff@gmail.com>
2026-02-03 18:37:25 -0500

    Completed tasks 1-4: routes for all books, ISBN, author, title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,63 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+   const book = books[isbn];           
+
+    if (book) {
+        res.send(book);                
+    } else {
+        res.status(404).json({          
+            message: "Book not found"
+        });
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;        
+    const keys = Object.keys(books);          
+    let result = [];
+
+    keys.forEach((key) => {
+        if (books[key].author.toLowerCase() === author.toLowerCase()) {
+            result.push(books[key]);
+        }
+    });
+
+    if (result.length > 0) {
+        res.send(result);                     
+    } else {
+        res.status(404).json({
+            message: "No books found for this author"
+        });
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;        
+    const keys = Object.keys(books);          
+    let result = [];
+
+    keys.forEach((key) => {
+        if (books[key].title.toLowerCase() === title.toLowerCase()) {
+            result.push(books[key]);
+        }
+    });
+
+    if (result.length > 0) {
+        res.send(result);                     
+    } else {
+        res.status(404).json({
+            message: "No books found with that title"
+        });
+    }
 });
 
 //  Get book review
