13c643ce836f2e1d03a24e016ed8de29a1563d35
Andre Fonseca <andrefonseca.aff@gmail.com>
2026-02-03 20:18:25 -0500

    Completed Task 9 - delete a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -85,6 +85,34 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization ? req.session.authorization.username: null;
+
+  if (!username) {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  if (book.reviews[username]) {
+    delete book.reviews[username];
+      return res.status(200).json({
+        message: `Review by ${username} deleted successfully`,
+        reviews: book.reviews
+      });
+      } else {
+        return res.status(404).json({
+            message: "No review found for this user on this book"
+      });
+    }
+});
+
+
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
